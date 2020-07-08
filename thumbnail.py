0fade49f35ad8053e468530d1e586080ba88055b
Fathimath Safvana K V <fathima111333777@gmail.com>
2020-07-08 09:30:54 +0530

    comments added

--- thumbnail.py
+++ thumbnail.py
@@ -1,6 +1,9 @@
+#importing image fom pil library
 from PIL import Image
+#created an object image
 image=Image.open("E:\wallpapers\wallpaper.jpg")
 maximum_size=(100,100)
 image.thumbnail(maximum_size)
+#ceating thumbnail
 image.show()
 image.save("thumbnailofwallpaper.png")
